f564554d987dd0337d6d63990cc8d367985bd8af
Prasadcoop <86963235+Prasadcoop@users.noreply.github.com>
2021-08-30 23:18:02 +0530

    Update keypad.ino

--- keypad/keypad.ino
+++ keypad/keypad.ino
@@ -6,8 +6,8 @@
 Servo myservo;
 int pos=0;
 
-const int ROW_NUM = 4; //four rows
-const int COLUMN_NUM = 3; //three columns
+const int ROW_NUM = 4;
+const int COLUMN_NUM = 3; 
 
 char keys[ROW_NUM][COLUMN_NUM] = {
   {'1','2','3'},
@@ -16,8 +16,8 @@ char keys[ROW_NUM][COLUMN_NUM] = {
   {'*','0','#'}
 };
 
-byte pin_rows[ROW_NUM] = {3,2,1,0}; //connect to the row pinouts of the keypad
-byte pin_column[COLUMN_NUM] = {4,5,6}; //connect to the column pinouts of the keypad
+byte pin_rows[ROW_NUM] = {3,2,1,0}; 
+byte pin_column[COLUMN_NUM] = {4,5,6}; 
 
 Keypad keypad = Keypad( makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM );
 
